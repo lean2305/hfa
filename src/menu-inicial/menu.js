219f36a9afc74ee5c3c439ed45c2dcda2bf5d65a
lean2305 <lo0184975@gmail.com>
2023-05-03 20:31:46 +0100

    v5de6

--- src/menu-inicial/menu.js
+++ src/menu-inicial/menu.js
@@ -71,15 +71,10 @@ class Quadrados extends Component{
                         <h1 className="quadrado_h1_menu_inicial">{this.props.quadrado_text_4}</h1>
                     </Link>
                     </div>
-
                     <div style={{backgroundImage:"url('https://img.freepik.com/fotos-gratis/mulher-de-negocio-que-trabalha-no-laptop_1388-91.jpg')"}} className="quadrado_maior_menu_inicial">
-                    <Link to="/" style={{ textDecoration: 'none' }}> 
-                        <svg className="svg_maior_menu_inicial" width="20%" height="20%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.09998 12V10.52C9.09998 8.60999 10.45 7.83999 12.1 8.78999L13.38 9.52999L14.66 10.27C16.31 11.22 16.31 12.78 14.66 13.73L13.38 14.47L12.1 15.21C10.45 16.16 9.09998 15.38 9.09998 13.48V12Z" stroke="#ffffff" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    </Link>
-                    </div>
+                    <video src={process.env.PUBLIC_URL + 'videom/hfa.mp4'} autoPlay loop muted style={{objectFit: 'cover', width: '100%', height: '100%'}} />
+</div>
+
 
                 </div>
             </div>
