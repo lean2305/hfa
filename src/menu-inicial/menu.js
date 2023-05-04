25d28ff8cca1814f5b5333a73c3cdffc27f79e49
lean2305 <lo0184975@gmail.com>
2023-05-04 19:49:12 +0100

    haf

--- src/menu-inicial/menu.js
+++ src/menu-inicial/menu.js
@@ -66,13 +66,13 @@ class Quadrados extends Component{
 
                 <div className="quadrado_fila2_menu_inicial">
                     <div style={{backgroundImage:"url('https://s1.static.brasilescola.uol.com.br/be/vestibular/notebook-ferramenta-trabalho-varios-profissionais-599844f1a665d.jpg')"}} className="quadrado_menor_menu_inicial">
-                    <Link to="/videos" style={{ textDecoration: 'none' }}> 
+                    <Link to="/" style={{ textDecoration: 'none' }}> 
                         <svg className="svg_menu_inicial" width="20%" height="20%" viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 9C2 6.79086 3.79086 5 6 5H13C15.2091 5 17 6.79086 17 9V9.07171L20.202 7.23108C21.0019 6.77121 22 7.34868 22 8.27144V15.7488C22 16.6203 21.1003 17.2012 20.306 16.8424L16.9855 15.3425C16.8118 17.3913 15.0938 19 13 19H6C3.79086 19 2 17.2091 2 15V9ZM17 13.1544L20 14.5096V9.65407L17 11.3786V13.1544ZM15 9C15 7.89543 14.1046 7 13 7H6C4.89543 7 4 7.89543 4 9V15C4 16.1046 4.89543 17 6 17H13C14.1046 17 15 16.1046 15 15V9Z" fill="#ffffff"/></svg>
                         <h1 className="quadrado_h1_menu_inicial">{this.props.quadrado_text_4}</h1>
                     </Link>
                     </div>
                     <div style={{backgroundImage:"url('https://img.freepik.com/fotos-gratis/mulher-de-negocio-que-trabalha-no-laptop_1388-91.jpg')"}} className="quadrado_maior_menu_inicial">
-                    <video src={process.env.PUBLIC_URL + 'video/hfa.mp4'} autoPlay loop muted style={{objectFit: 'cover', width: '100%', height: '100%'}} />
+                    <video src={process.env.PUBLIC_URL + 'videom/hfa.mp4'} autoPlay loop muted style={{objectFit: 'cover', width: '100%', height: '100%'}} />
 </div>
 
 
